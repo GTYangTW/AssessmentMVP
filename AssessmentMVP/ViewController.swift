5d32b0c79caa94e121e613f2e1b1faef440ffb13
GTYang <gtq.yang@gmail.com>
2024-02-06 08:40:16 +0800

    first commit

---
+++ AssessmentMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentMVP
+//
+//  Created by 10362 on 2024/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
