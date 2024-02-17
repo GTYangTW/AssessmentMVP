a375e5483e71cc41ff9210329e1a67e5e6f1fc8b
GTYang <gtq.yang@gmail.com>
2024-02-17 16:33:46 +0800

    feat: 按鈕翻頁機制與底線完成

--- AssessmentMVP/View/PageVC.swift
+++ AssessmentMVP/View/PageVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class PageVC: UIPageViewController, UIPageViewControllerDelegate {
     
+    let lbTitleRed = UILabel()
+    let lbTitleWhite = UILabel()
+    let simulatorSegment = UIView()
+    
     var currentOfPage: Int = 0
     let mainV = MainView()
     let secV: UIViewController = {
@@ -33,7 +37,7 @@ class PageVC: UIPageViewController, UIPageViewControllerDelegate {
     lazy var arrayBtn : [UIButton] = [btn1, btn2, btn3]
     var btn1: UIButton {
         let btn = UIButton()
-        btn.setTitle(" wero ", for: .normal) //MainView
+        btn.setTitle(" Raners ", for: .normal) //MainView
         return btn
     }
     var btn2: UIButton {
@@ -61,16 +65,86 @@ class PageVC: UIPageViewController, UIPageViewControllerDelegate {
         setViewControllers([arrayVC[0]], direction: .forward, animated: false, completion: nil)
         self.setupBasic()
         self.setupButton()
-        view.backgroundColor = .brown
         // Do any additional setup after loading the view.
     }
     func setupBasic(){
-        //..
+        view.addSubview(lbTitleRed)
+        view.addSubview(lbTitleWhite)
+        lbTitleRed.text = "Red"
+        lbTitleWhite.text = "So"
+        lbTitleRed.textColor = UIColor.red
+        lbTitleWhite.textColor = UIColor.white
+        lbTitleRed.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        lbTitleWhite.font = UIFont.systemFont(ofSize: 32, weight: .regular)
+        
+        lbTitleRed.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.right.equalTo(view.snp.centerX)
+        }
+        lbTitleWhite.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.equalTo(lbTitleRed.snp.right)
+        }
     }
     func setupButton(){
-        //..
+        view.addSubview(simulatorSegment)
+        simulatorSegment.backgroundColor = .black
+        for i in arrayBtn{
+            i.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+            i.setTitleColor(.white, for: .normal )
+            i.layer.borderWidth = 1.0
+            i.setTitleColor(UIColor.white, for: .highlighted)
+            i.addTarget(self, action: #selector(self.btnDidTap(_:)), for: .touchUpInside)
+            simulatorSegment.addSubview(i)
+        }
+        var previousButton: UIButton?
+        for button in arrayBtn {
+            button.snp.makeConstraints { make in
+                make.top.equalToSuperview()
+                make.height.equalToSuperview().multipliedBy(0.8)
+                make.width.equalTo(button.intrinsicContentSize.width)
+                if let previousButton = previousButton {
+                    make.left.equalTo(previousButton.snp.right)
+                } else {
+                    make.left.equalToSuperview()
+                }
+            }
+            previousButton = button
+        }
+        arrayBtn[currentOfPage].addUnderline()
+        simulatorSegment.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.width.equalToSuperview()
+            make.top.equalTo(self.lbTitleRed.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
     }
     
+    @objc
+    func btnDidTap(_ sender: UIButton) {
+        var direction = NavigationDirection.forward
+        animateUnderLine(sender)
+        if let index = arrayBtn.firstIndex(of: sender){
+            switch currentOfPage > index {
+            case true:
+                direction = NavigationDirection.reverse
+            case false:
+                direction = NavigationDirection.forward
+            }
+            //pagecontrol 跳轉頁面控制功能，直接跳頁數
+            setViewControllers([arrayVC[index]], direction: direction, animated: true, completion: nil)
+            currentOfPage = index
+        }
+    }
+    func animateUnderLine(_ button: UIButton) {
+        for btn in simulatorSegment.subviews {
+            if let otherButton = btn as? UIButton,
+               otherButton != button {
+                otherButton.removeUnderline()
+            }
+        }
+        button.addUnderline()
+    }
     /*
     // MARK: - Navigation
 
@@ -103,4 +177,47 @@ extension PageVC : UIPageViewControllerDataSource {
     }
     
     
+}
+extension UIButton {
+    // 添加底線
+    func removeUnderline() {
+        guard let text = self.titleLabel?.text else { return }
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.removeAttribute(
+            NSAttributedString.Key.underlineStyle,
+            range: NSRange(location: 0, length: text.count)
+        )
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+    func addUnderline() {
+        guard let text = self.titleLabel?.text else { return }
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(
+            NSAttributedString.Key.underlineStyle,
+            value: NSUnderlineStyle.thick.rawValue,
+            range: NSRange(location: 0, length: text.count)
+        )
+        attributedString.addAttribute(
+            NSAttributedString.Key.underlineColor,
+            value: UIColor.white,
+            range: NSRange(location: 0, length: text.count)
+        )
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+    /*
+    func addUnderline(for label: UILabel?) {
+        guard let titleLabel = label else { return }
+        let attributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        let attributedString = NSAttributedString(string: titleLabel.text ?? "", attributes: attributes)
+        titleLabel.attributedText = attributedString
+    }
+    func addBottomBorder(color: UIColor, thickness: CGFloat) {
+        let border = CALayer()
+        border.backgroundColor = color.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - thickness, width: self.frame.size.width, height: thickness)
+        self.layer.addSublayer(border)
+    }
+     */
 }
---
+++ AssessmentMVP/View/UnderlineButton.swift
@@ -0,0 +1,31 @@
+//
+//  UnderlineButton.swift
+//  AssessmentMVP
+//
+//  Created by 10362 on 2024/2/17.
+//
+
+import Foundation
+import UIKit
+
+class UnderlinedButton: UIButton {
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        if let titleLabel = titleLabel {
+            let textRect = titleLabel.frame
+            
+            // 設置底線的位置
+            let lineY = textRect.origin.y + textRect.size.height
+            let lineWidth = textRect.size.width
+            
+            // 繪製底線
+            let context = UIGraphicsGetCurrentContext()
+            context?.setStrokeColor(UIColor.white.cgColor)
+            context?.setLineWidth(1.0)
+            context?.move(to: CGPoint(x: textRect.origin.x, y: lineY))
+            context?.addLine(to: CGPoint(x: textRect.origin.x + lineWidth, y: lineY))
+            context?.strokePath()
+        }
+    }
+}
